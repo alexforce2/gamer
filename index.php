00354ffc7a5c227189532ee1e9088a49fdc37306
VasiliyBaranov <backahell@rambler.ru>
2014-03-21 10:11:37 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$start = microtime(true);
+header('Content-Type: text/html; charset=utf-8');
+date_default_timezone_set('UTC');
+error_reporting(2047);
+ini_set('display_errors', 0);
+require_once 'application/bootstrap.php';
+//echo 'Время выполнения скрипта: ' . (microtime(true) - $start) . ' сек.<br />';
